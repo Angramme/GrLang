a3d0af1ac700310116f49c400ccbed3e07716965
Angramme <kacpero376@gmail.com>
2021-03-03 22:36:34 +0100

    fixed build errors

--- BuildConfig.cmake
+++ BuildConfig.cmake
@@ -1,4 +1,5 @@
 
 
-set(LLVM_INCLUDE "D:/dev/LLVM/llvm-10.0.1-build/include")
+set(LLVM_INCLUDE "D:/dev/LLVM/llvm-10.0.1/include")
+set(LLVM_BUILD_INCLUDE "D:/dev/LLVM/llvm-10.0.1-build/include")
 set(LLVM_LIB "D:/dev/LLVM/llvm-10.0.1-build/Release/lib")
