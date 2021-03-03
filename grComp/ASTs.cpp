25f5358d4663266a6951f027c9096360357fdfec
Angramme <kacpero376@gmail.com>
2021-03-03 22:24:52 +0100

    implemented asts

--- grComp/ASTs.cpp
+++ grComp/ASTs.cpp
@@ -2,15 +2,44 @@
 #include <string>
 
 namespace grComp {
-	std::string ExprASTtoStr(const ExprAST* ast) {
-		switch (ast->expr_type)
-		{
-		default:
-			break;
-		case ExprAstType::BinaryExprAST_t: return BinaryExprASTtoStr(static_cast<const BinaryExprAST*>(ast)); break;
-		case ExprAstType::NumberExprAST_t: return NumberExprASTtoStr(static_cast<const NumberExprAST*>(ast)); break;
-		}
-	}
+
+	BaseAST::BaseAST(const AstType& expr_type)
+		: expr_type(expr_type)
+	{}
+
+	ExprAST::ExprAST(const AstType& expr_type)
+		: BaseAST(expr_type)
+	{}
+
+	NumberExprAST::NumberExprAST(double Val)
+		: ValR(Val), ExprAST(AstType::NumberExprAST_t) 
+	{}
+
+	VariableExprAST::VariableExprAST(const std::string& Name)
+		: Name(Name), ExprAST(AstType::VariableExprAST_t)
+	{}
+
+	BinaryExprAST::BinaryExprAST(signed short op, std::unique_ptr<ExprAST> LHS,
+		std::unique_ptr<ExprAST> RHS)
+		: Op(op), LHS(std::move(LHS)), RHS(std::move(RHS)), ExprAST(AstType::BinaryExprAST_t)
+	{}
+
+	CallExprAST::CallExprAST(const std::string &Callee,
+		std::vector<std::unique_ptr<ExprAST>> Args)
+		: Callee(Callee), Args(std::move(Args)), ExprAST(AstType::CallExprAST_t)
+	{}
+
+	PrototypeAST::PrototypeAST(const std::string &name, std::vector<std::string> Args)
+		: Name(name), Args(std::move(Args)), BaseAST(AstType::PrototypeAST_t)
+	{}
+
+	FunctionAST::FunctionAST(std::unique_ptr<PrototypeAST> Proto, std::unique_ptr<ExprAST> Body)
+		: Proto(std::move(Proto)), Body(std::move(Body)), BaseAST(AstType::FunctionAST_t)
+	{}
+
+
+
+	// to string conversions
 
 	static std::string BinaryExprASTtoStr(const BinaryExprAST* ast) {
 		return ExprASTtoStr(ast->LHS.get()) + (char)ast->Op + ExprASTtoStr(ast->RHS.get());
@@ -18,5 +47,15 @@ namespace grComp {
 
 	static std::string NumberExprASTtoStr(const NumberExprAST* ast) {
 		return std::to_string(ast->ValR);
+	}
+
+	std::string ASTtoStr(const BaseAST* ast) {
+		switch (ast->get_ast_type())
+		{
+		default:
+			break;
+		case AstType::BinaryExprAST_t: return BinaryExprASTtoStr(static_cast<const BinaryExprAST*>(ast)); break;
+		case AstType::NumberExprAST_t: return NumberExprASTtoStr(static_cast<const NumberExprAST*>(ast)); break;
+		}
 	}
 }
--- grComp/ASTs.h
+++ grComp/ASTs.h
@@ -5,24 +5,39 @@
 #include "Type.h"
 
 namespace grComp {
-	enum class ExprAstType : char {
+	enum class AstType : char {
+		ErrorAST_t = 0,
+
 		ExprAST_t,
 		NumberExprAST_t,
 		VariableExprAST_t,
 		BinaryExprAST_t,
-		CallExprAST_t
+		CallExprAST_t,
+
+		PrototypeAST_t,
+		FunctionAST_t,
+	};
+
+	//base class for all ASTs
+	class BaseAST {
+	private:
+		const AstType expr_type = AstType::ErrorAST_t;
+	protected:
+		BaseAST(const AstType& expr_type);
+	public:
+		AstType get_ast_type() inline const { return expr_type;  }
 	};
 
 
-	class ExprAST {
+	class ExprAST : public BaseAST {
 	public:
 		std::shared_ptr<grComp::Type> Type;
 		//virtual ~ExprAST() {}
-		static const ExprAstType expr_type = ExprAstType::ExprAST_t;
+	protected:
+		ExprAST(const AstType& expr_type);
 	};
 
 	/// NumberExprAST - Expression class for numeric literals like "1.0".
-	const ExprAstType NumberExprAST::expr_type = ExprAstType::NumberExprAST_t;
 	class NumberExprAST : public ExprAST {
 	public:
 		union {
@@ -31,69 +46,63 @@ namespace grComp {
 			unsigned long long ValN;
 		};
 
-		NumberExprAST(double Val) : ValR(Val) {}
+		NumberExprAST(double Val);
 		/*NumberExprAST(long long Val) : ValZ(Val) {}
 		NumberExprAST(unsigned long long Val) : ValN(Val) {}*/
 
 	};
 
 	/// VariableExprAST - Expression class for referencing a variable, like "a".
-	const ExprAstType VariableExprAST::expr_type = ExprAstType::VariableExprAST_t;
 	class VariableExprAST : public ExprAST {
 	public:
 		std::string Name;
 
-		VariableExprAST(const std::string &Name) : Name(Name) {}
+		VariableExprAST(const std::string &Name);
 	};
 
 	/// BinaryExprAST - Expression class for a binary operator.
-	const ExprAstType BinaryExprAST::expr_type = ExprAstType::BinaryExprAST_t;
 	class BinaryExprAST : public ExprAST {
 	public:
 		signed short Op;
 		std::unique_ptr<ExprAST> LHS, RHS;
 
 		BinaryExprAST(signed short op, std::unique_ptr<ExprAST> LHS,
-			std::unique_ptr<ExprAST> RHS)
-			: Op(op), LHS(std::move(LHS)), RHS(std::move(RHS)) {}
+			std::unique_ptr<ExprAST> RHS);
 		
 	};
 
 	/// CallExprAST - Expression class for function calls.
-	const ExprAstType CallExprAST::expr_type = ExprAstType::CallExprAST_t;
 	class CallExprAST : public ExprAST {
 	public:
 		std::string Callee;
 		std::vector<std::unique_ptr<ExprAST>> Args;
 
 		CallExprAST(const std::string &Callee,
-			std::vector<std::unique_ptr<ExprAST>> Args)
-			: Callee(Callee), Args(std::move(Args)) {}
+			std::vector<std::unique_ptr<ExprAST>> Args);
 	};
 
 
 	/// PrototypeAST 
-	//const ExprAstType PrototypeAST::expr_type = ExprAstType::PrototypeAST_t;
-	class PrototypeAST {
+	class PrototypeAST : BaseAST {
 	public:
 		std::string Name;
 		std::vector<std::string> Args;
 
-		PrototypeAST(const std::string &name, std::vector<std::string> Args)
-			: Name(name), Args(std::move(Args)) {}
+		PrototypeAST(const std::string &name, std::vector<std::string> Args);
 
 		const std::string &getName() const { return Name; }
 	};
 
 	/// FunctionAST - This class represents a function definition itself.
-	//const ExprAstType FunctionAST::expr_type = ExprAstType::FunctionAST_t;
-	class FunctionAST {
+	class FunctionAST : BaseAST {
 	public:
 		std::unique_ptr<PrototypeAST> Proto;
 		std::unique_ptr<ExprAST> Body;
 
 		FunctionAST(std::unique_ptr<PrototypeAST> Proto,
-			std::unique_ptr<ExprAST> Body)
-			: Proto(std::move(Proto)), Body(std::move(Body)) {}
+			std::unique_ptr<ExprAST> Body);
 	};
+
+
+	std::string ExprASTtoStr(const ExprAST* ast);
 }
