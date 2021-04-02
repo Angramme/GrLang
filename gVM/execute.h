32d0bc1a639a1cdb882b56d29f8a41d62aed2c4b
Angramme <kacpero376@gmail.com>
2021-04-03 00:07:52 +0200

    minor changes (changed includes and moved files to appropriate places)

--- gVM/execute.h
+++ gVM/execute.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include "Memory.h"
-#include "Instructions.h"
+#include "core/Instructions.h"
 
 namespace gVM {
 	void execute_program(char* instructions, size_t length, size_t start_at, StackMemory* smem);
