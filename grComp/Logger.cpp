e6431ee221708a249bcb529ae75c306647094d0a
Angramme <kacpero376@gmail.com>
2021-06-19 20:34:36 +0200

    updated logger

--- grComp/Logger.cpp
+++ grComp/Logger.cpp
@@ -1,6 +1,7 @@
 #include "Logger.h"
 
 #include <iostream>
+#include <stdio.h>
 
 #include "grlangConfig.h"
 
@@ -11,53 +12,72 @@
 
 namespace grComp {
 	namespace Log {
-		static enum COLOR {
-			red=0, green=1, blue, yellow, magenta, cyan, black, white
-		};
 
-		static void LOG(std::string msg, COLOR clr) {
+		static WORD _Default_Color_Attributes = 0;
+
+		void PRINT_COLOR(const std::string& str, const COLOR& color, FILE* STDOUT) {
+			return PRINT_COLOR(str.c_str(), color, STDOUT);
+		}
+
+		void PRINT_COLOR(const char* msg, const COLOR& clr, FILE* STDOUT) {
 #if defined PLATFORM_UNIX
-			const short linux_colors[] = { 31, 32, 34, 33, 35, 36, 30, 37 };
-			printf("\033[%dm%s", linux_colors[clr], msg.c_str());
+			if (clr != COLOR::DEFAULT) {
+				const short linux_colors[] = { 31, 32, 34, 33, 35, 36, 30, 37 };
+				fprintf(STDOUT, "\033[%dm%s\033[39m", linux_colors[clr-1], msg);
+			}
+			else {
+				fprintf(STDOUT, "\033[39m%s", msg);
+			}
 
 #elif defined PLATFORM_WIN32
-			HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-			auto CL = 0;
-			if(clr==red || clr == yellow || clr == magenta)
-				CL |= FOREGROUND_RED;
-			if (clr == green || clr == yellow || clr == cyan)
-				CL |= FOREGROUND_GREEN;
-			if (clr == blue || clr == magenta || clr == cyan)
-				CL |= FOREGROUND_BLUE;
+			if (clr != DEFAULT) {
+				if (!_Default_Color_Attributes) {
+					CONSOLE_SCREEN_BUFFER_INFO Info;
+					GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &Info);
+					_Default_Color_Attributes = Info.wAttributes;
+				}
 
-			SetConsoleTextAttribute(hConsole,
-				CL);
-			printf("%s\n", msg.c_str());
+				HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+				auto CL = FOREGROUND_INTENSITY;
+				if(clr==red || clr == yellow || clr == magenta)
+					CL |= FOREGROUND_RED;
+				if (clr == green || clr == yellow || clr == cyan)
+					CL |= FOREGROUND_GREEN;
+				if (clr == blue || clr == magenta || clr == cyan)
+					CL |= FOREGROUND_BLUE;
+
+				SetConsoleTextAttribute(hConsole, CL);
+				fprintf(STDOUT, "%s\n", msg);
+				SetConsoleTextAttribute(hConsole, _Default_Color_Attributes);
+			}
+			else {
+				fprintf(STDOUT, "%s\n", msg);
+			}
 #endif
 
 		}
 		void InternalRuntimeError(std::string msg, const char* in_file, int on_line, const char* in_function) {
-			LOG("\n!!==!!==!!==!!==!!==!!\n!!==!!==!!==!!==!!==!!", yellow);
-			LOG(
+			PRINT_COLOR("\n!!==!!==!!==!!==!!==!!\n!!==!!==!!==!!==!!==!!", yellow, stderr);
+			PRINT_COLOR(
 				(std::string)"Internal Compiler Runtime Error: \n" +
 				"\tin file: " + in_file +
 				"\n\ton line: " + std::to_string(on_line) +
 				"\n\tin function: " + in_function + "\n" +
 				msg +
 				"\n This is abnormal behaviour. Please report this error to the GrLang project maintainer.",
-				magenta
+				magenta, stderr
 			);
-			LOG("!!==!!==!!==!!==!!==!!\n!!==!!==!!==!!==!!==!!\n", yellow);
+			PRINT_COLOR("!!==!!==!!==!!==!!==!!\n!!==!!==!!==!!==!!==!!\n", yellow, stderr);
 		}
 		void CompiletimeError(std::string msg) {
-			LOG(
-				"Compile Error: " + msg,
+			PRINT_COLOR(
+				msg,
 				red
 			);
 		}
 		void CompiletimeInfo(std::string msg) {
-			LOG(
-				"Compile info: " + msg,
+			PRINT_COLOR(
+				msg,
 				blue
 			);
 		}
--- grComp/Logger.h
+++ grComp/Logger.h
@@ -1,7 +1,15 @@
+#pragma once
+
 #include <string>
 
 namespace grComp {
 	namespace Log {
+		enum COLOR {
+			DEFAULT=0, red = 1, green = 2, blue, yellow, magenta, cyan, black, white
+		};
+		void PRINT_COLOR(const char* msg, const COLOR& color, FILE* STDOUT = stdout);
+		void PRINT_COLOR(const std::string& str, const COLOR& color, FILE* STDOUT = stdout);
+
 		void InternalRuntimeError(std::string msg, const char* in_file, const int on_line, const char* in_function);
 		void CompiletimeError(std::string msg);
 		void CompiletimeInfo(std::string msg);
